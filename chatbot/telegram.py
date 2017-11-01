3b1891db1df62392909cbb8b6b1872da9168c731
WilliamGomes <gomes.souza@live.com>
2017-11-01 17:51:31 -0200

    Pasta

---
+++ chatbot/telegram.py
@@ -0,0 +1,20 @@
+#telepot library
+import telepot 
+
+#method to connect your bot from your token 
+bot = telepot.Bot("463791509:AAH_naZeg_zYzzn_tIgioayaWsBCKBqJX20")
+
+
+#A function who get the message and return to your terminal 
+def recebendoMsg(msg):
+	print(msg['text'])
+
+#Every time someone text to the bot refreshing the function sending a new message.   
+bot.message_loop(recebendoMsg)
+
+
+while True:
+	pass
+
+
+#This python file get a mesage send for your bot and return that message on temrminal
