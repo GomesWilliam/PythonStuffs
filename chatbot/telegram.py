b7d4f4b9182033faf29a061fa200d2249685a6d9
WilliamGomes <gomes.souza@live.com>
2017-11-06 13:34:38 -0200

    Modificado

--- chatbot/telegram.py
+++ chatbot/telegram.py
@@ -7,7 +7,11 @@ bot = telepot.Bot("463791509:AAH_naZeg_zYzzn_tIgioayaWsBCKBqJX20")
 
 #A function who get the message and return to your terminal 
 def recebendoMsg(msg):
-	print(msg['text'])
+	chat_id = msg['chat']['id']
+	texto = 'Ola, '+msg['from']['first_name']+'!'
+	bot.sendMessage(chat_id, texto)
+
+#print(msg['text'])
 
 #Every time someone text to the bot refreshing the function sending a new message.   
 bot.message_loop(recebendoMsg)
